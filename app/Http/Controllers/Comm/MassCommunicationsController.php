d75e736622691ba157d1a816609f3b27234fb47c
Angel Gonzalez <angel@capeandbay.com>
2021-11-18 18:41:40 -0500

    Mass Comms Dashboard Empty State

--- app/Http/Controllers/Comm/MassCommunicationsController.php
+++ app/Http/Controllers/Comm/MassCommunicationsController.php
@@ -13,11 +13,49 @@ class MassCommunicationsController extends Controller
         $client_id = request()->user()->currentClientId();
         $is_client_user = request()->user()->isClientUser();
 
+        if(!is_null($client_id))
+        {
+            $aud_options = [
+                'all' => 'All Audiences',
+                'prospects' => 'Prospects',
+                'conversions' => 'Conversions'
+            ];
+        }
+        else
+        {
+            $aud_options = [
+                'all' => 'All Audiences',
+                'admins' => 'Cape & Bay Admins',
+                'employees' => 'Cape & Bay Non-Admins'
+            ];
+        }
+
         $active_audience = 'all';
+        if(request()->has('audience'))
+        {
+            $aud = (request()->get('audience'));
+            switch($aud)
+            {
+                case 'all':
+                    //@todo - what ever is needed to filfill this need
+                    $active_audience = $aud;
+                    break;
+                case 'prospects':
+                case 'conversions':
+                case 'admins':
+                case 'employees':
+                    //@todo - what ever is needed to filfill this need
+                    $active_audience = $aud;
+                break;
+
+                default:
+                    // @todo - this will be a uuid to be looked up
+            }
+        }
 
         return Inertia::render('Comms/MassCommsDashboard', [
             'title' => 'Mass Communications',
-            'audiences' => ['all' => "All Audiences"],
+            'audiences' => $aud_options,
             'activeAudience' => $active_audience
         ]);
     }
--- resources/js/Pages/Comms/MassCommsDashboard.vue
+++ resources/js/Pages/Comms/MassCommsDashboard.vue
@@ -17,7 +17,7 @@
                             <template #trigger>
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                                        Audiences
+                                        {{ (activeAudience in audiences) ? 'Audience: ' +audiences[activeAudience] : 'Audiences'}}
 
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -37,7 +37,7 @@
 
                                         <div class="h-40 lg:h-20 overflow-y-scroll">
                                             <template v-for="(lbl, slug) in audiences" :key="slug">
-                                                <form @submit.prevent="comingSoon(location)">
+                                                <form @submit.prevent="comingSoon()">
                                                     <jet-dropdown-link as="button">
                                                         <div class="flex items-center">
                                                             <svg v-if="activeAudience === slug" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
@@ -59,29 +59,29 @@
                         <div class="mr-1">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
+                                href="#" @click="comingSoon()">
                                 <span>Emails Templates <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
                             </Link>
                         </div>
                         <div class="mr-1 ">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
-                                <span>SMS Templates <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
+                                href="#" @click="comingSoon()">
+                            <span>SMS Templates <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
                             </Link>
                         </div>
                         <div class="mr-1 mt-1 sm:mt-0">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
-                                <span>Email Campaigns <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
+                                href="#" @click="comingSoon()">
+                            <span>Email Campaigns <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
                             </Link>
                         </div>
                         <div class="mt-1 md:mt-0">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
-                                <span>SMS Campaigns <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
+                                href="#" @click="comingSoon()">
+                            <span>SMS Campaigns <span class="bg-info p-1">0</span> <span class=" p-1 bg-success">0</span></span>
                             </Link>
                         </div>
                     </div>
@@ -90,22 +90,50 @@
                         <div class="mt-2 mr-1 xl:mt-0">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
-                                <span>+ New Email</span>
+                                href="#" @click="comingSoon()">
+                            <span>+ New Email</span>
                             </Link>
                         </div>
                         <div class="mt-2 ml-1 xl:mt-0">
                             <Link
                                 class="btn justify-self-end"
-                                :href="route('data.leads.create')">
-                                <span>+ New SMS</span>
+                                href="#" @click="comingSoon()">
+                            <span>+ New SMS</span>
                             </Link>
                         </div>
                     </div>
                 </div>
-                <div class="comms-content"></div>
-                </div>
+            </div>
             <div class="border-b-4 border-blue-300"></div>
+            <!-- Page Content -->
+            <div class="comms-content mt-4 flex flex-col lg:flex-row lg-w-full">
+                <div class="left-section flex flex-col lg-w-30 mb-3 lg:mb-0 lg:mr-3">
+                    <div class="total-audience-breakdown border-2 border-gray-300">
+                        <div class="flex flex-col">
+                            <h2 class="text-gray-800 m-2"> Total Audience</h2>
+                            <div class="text-center">
+                                <h1 class="text-gray-800 ">0</h1>
+                            </div>
+                            <div class="border-b-2 border-gray-300 m-2"></div>
+                            <h2 class="text-gray-800 m-2"> Total Audience Breakdown</h2>
+                            <div v-for="(lbl, slug) in audiences">
+                                 <p class="m-2"><b>{{ lbl }}</b> : 0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right-section flex flex-col lg-w-70 mt-3 lg:mt-0 lg:ml-3">
+                    <div class="current-feed border-2 border-gray-300">
+                        <div class="bg-blue-200 border-b-2 border-gray-300 py-2">
+                            <h1 class="ml-2 bg-blue-200">Your Feed</h1>
+                        </div>
+                        <div class="text-center">
+                            There's nothing here! Do something about it.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </jet-bar-container>
     </app-layout>
 </template>
@@ -155,6 +183,20 @@ export default defineComponent({
 
 <style scoped>
 
+
+    @media (min-width: 1024px) {
+        .lg-w-full {
+            width: 100%;
+        }
+
+        .lg-w-30 {
+            width: 30%;
+        }
+
+        .lg-w-70 {
+            width: 70%;
+        }
+    }
 
     @media (min-width: 1280px) {
         .xl-justify-left {

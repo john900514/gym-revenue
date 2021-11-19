0124079a62aff9676452c01b3567db8b18c5ac91
Angel Gonzalez <angel@capeandbay.com>
2021-11-19 09:04:38 -0500

    Companion Pages to Mass Comm

---
+++ resources/js/Pages/Comms/Emails/Templates/EmailTemplatesIndex.vue
@@ -0,0 +1,49 @@
+<template>
+    <app-layout :title="title">
+        <template #header>
+            <div class="text-center">
+                <h2 class="font-semibold text-xl  leading-tight">
+                    Email Template Management
+                </h2>
+                <small></small>
+            </div>
+        </template>
+    </app-layout>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import {Link} from '@inertiajs/inertia-vue3';
+import AppLayout from '@/Layouts/AppLayout.vue'
+import JetDropdown from '@/Jetstream/Dropdown'
+import JetDropdownLink from '@/Jetstream/DropdownLink'
+import JetBarContainer from "@/Components/JetBarContainer";
+
+export default defineComponent({
+    name: "EmailTemplatesIndex",
+    components: {
+        Link,
+        AppLayout,
+        JetDropdown,
+        JetDropdownLink,
+        JetBarContainer
+    },
+    props: ['title'],
+    setup(props) {},
+    watch: {},
+    data() {
+        return {};
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    mounted() {}
+});
+</script>
+
+<style scoped>
+
+</style>

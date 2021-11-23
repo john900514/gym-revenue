0fc8b7d5662efa431b5657e9441c7d6bb9b4f0b7
Philip Krogel <philip@capeandbay.com>
2021-11-23 18:59:51 -0500

    setup so we can use shades of primary/secondary/accent colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,52 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
 
-const colors = require('tailwindcss/colors');
+const twColors = require('tailwindcss/colors');
 //add default to colors so we don't always need -500 for base shade
-Object.entries(colors).forEach(([name, color]) => color.DEFAULT = color[500]);
+Object.entries(twColors).forEach(([name, color]) => color.DEFAULT = color[500]);
+
+const colors = {
+    ...twColors,
+    primary: {
+        DEFAULT: '#073A76',
+        '50': '#98C4F9',
+        '100': '#7BB4F7',
+        '200': '#4193F4',
+        '300': '#0E73EA',
+        '400': '#0A56B0',
+        '500': '#073A76',
+        '600': '#052C59',
+        '700': '#041E3C',
+        '800': '#020F1F',
+        '900': '#000102'
+    },
+    secondary: {
+        DEFAULT: '#0074C8',
+        '50': '#AFDDFF',
+        '100': '#95D2FF',
+        '200': '#62BDFF',
+        '300': '#2FA8FF',
+        '400': '#0092FB',
+        '500': '#0074C8',
+        '600': '#00599A',
+        '700': '#003F6C',
+        '800': '#00243E',
+        '900': '#000A10'
+    },
+    accent: {
+        DEFAULT: '#82D300',
+        '50': '#D3FF8C',
+        '100': '#CBFF77',
+        '200': '#BBFF4E',
+        '300': '#ACFF26',
+        '400': '#9BFC00',
+        '500': '#82D300',
+        '600': '#6CAF00',
+        '700': '#568C00',
+        '800': '#406800',
+        '900': '#2A4400'
+    }
+}
 
 module.exports = {
     dark: false,
@@ -34,7 +77,7 @@ module.exports = {
         // require('@tailwindcss/forms'),
         // require('@tailwindcss/typography'),
         require('daisyui'),
-        plugin(function({ addUtilities }) {
+        plugin(({addUtilities}) => {
             const newUtilities = {
                 '.position-unset': {
                     position: 'unset',
@@ -46,17 +89,17 @@ module.exports = {
     daisyui: {
         themes: [{
             dark: {
-                'primary': '#073a76',
-                'primary-focus': '#0154ad',
+                'primary': colors.primary[500],
+                'primary-focus': colors.primary[400],
                 'primary-content': '#ffffff',
-                'secondary': '#0074C8',
-                'secondary-focus': '#0092fe',
+                'secondary': colors.secondary[500],
+                'secondary-focus': colors.secondary[600],
                 'secondary-content': '#ffffff',
-                'accent': '#82D300',
-                'accent-focus': '#99f800',
+                'accent': colors.accent[500],
+                'accent-focus': colors.accent[600],
                 'accent-content': '#ffffff',
                 'neutral': '#1c1c1c',
-                'neutral-focus': '#073a76',
+                'neutral-focus': colors.primary[500],
                 'neutral-content': '#ffffff',
                 'base-100': '#242424',
                 'base-200': '#202020',

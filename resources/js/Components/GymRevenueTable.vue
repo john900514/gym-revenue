c25f25a130d3926e832db1934dfb27906064155c
Philip Krogel <philip@capeandbay.com>
2021-11-23 19:27:28 -0500

    fix table on mobile

--- resources/js/Components/GymRevenueTable.vue
+++ resources/js/Components/GymRevenueTable.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex flex-col">
-        <div class="-my-2 sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow border-b border-base-100 bg-base-300">
+    <div class="flex flex-col  m">
+        <div class="-my-2 sm:-mx-6 lg:-mx-8 ax-w-screen overflow-x-scroll">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 ">
+                <div class="shadow border-b border-base-100 bg-base-300 ">
                     <slot name="prethead"/>
                     <div class="py-4 px-4">
-                        <table class="table min-w-full " :class="{'table-zebra': zebra}">
+                        <table class="table" :class="{'table-zebra': zebra}">
                             <thead>
                             <tr>
                                 <th v-for="(header, index) in headers" :key="index" scope="col">

c52de64a2ff984eb5629394e1d09d587f6b13222
Philip Krogel <philip@capeandbay.com>
2021-12-07 12:55:03 -0500

    add filters to the crud class

--- resources/js/Components/CRUD/GymRevenueCrud.vue
+++ resources/js/Components/CRUD/GymRevenueCrud.vue
@@ -1,25 +1,67 @@
 <template>
-    <div class="hidden lg:block">
-        <gym-revenue-data-table v-bind="$props"  />
-    </div>
-    <div class="lg:hidden">
-        <gym-revenue-data-cards v-bind="$props"/>
-    </div>
-    <slot name="pagination">
-        <pagination class="mt-4" :links="resource.links" />
-    </slot>
+    <jet-bar-container class="relative">
+        <div class="flex flex-row items-center mb-4">
+            <slot name="filter">
+                <search-filter
+                    v-model:modelValue="form.search"
+                    class="w-full max-w-md mr-4"
+                    @reset="reset"
+                >
+                    <div class="block py-2 text-xs text-gray-400">Trashed:</div>
+                    <select
+                        v-model="form.trashed"
+                        class="mt-1 w-full form-select"
+                    >
+                        <option :value="null" />
+                        <option value="with">With Trashed</option>
+                        <option value="only">Only Trashed</option>
+                    </select>
+                </search-filter>
+            </slot>
+            <div class="flex-grow" />
+            <slot name="top-actions">
+                <button
+                    v-for="action in Object.values(topActions)"
+                    class="btn"
+                    :class="action.class"
+                    @click.prevent="() => action.handler({ data, baseUrl })"
+                >
+                    {{ action.label }}
+                </button>
+            </slot>
+        </div>
+
+        <div class="hidden lg:block">
+            <gym-revenue-data-table v-bind="$props" />
+        </div>
+        <div class="lg:hidden">
+            <gym-revenue-data-cards v-bind="$props" />
+        </div>
+        <slot name="pagination">
+            <pagination class="mt-4" :links="resource.links" />
+        </slot>
+    </jet-bar-container>
 </template>
 
 <script>
+import { ref, defineComponent, watch } from "vue";
 import Pagination from "@/Components/Pagination";
 import GymRevenueDataCards from "./GymRevenueDataCards";
 import GymRevenueDataTable from "./GymRevenueDataTable";
+import SearchFilter from "@/Components/SearchFilter";
+import JetBarContainer from "@/Components/JetBarContainer";
+import pickBy from "lodash/pickBy";
+import throttle from "lodash/throttle";
+import { Inertia } from "@inertiajs/inertia";
+import { merge } from "lodash";
 
-export default {
+export default defineComponent({
     components: {
         GymRevenueDataCards,
         GymRevenueDataTable,
         Pagination,
+        SearchFilter,
+        JetBarContainer,
     },
     props: {
         fields: {
@@ -48,6 +90,41 @@ export default {
             type: Object,
             default: {},
         },
+        topActions: {
+            type: Object,
+            default: {},
+        },
+        baseUrl: {
+            type: String,
+        },
     },
-};
+    setup(props) {
+        const form = ref({});
+        let __baseUrl =
+            props.baseUrl || props.modelNamePlural || props.modelName + "s";
+
+        const formHandler = throttle(function () {
+            Inertia.get(route(__baseUrl), pickBy(form.value), {
+                preserveState: true,
+                preserveScroll: true,
+            });
+        }, 150);
+        watch([form], formHandler, { deep: true });
+        const defaultTopActions = {
+            create: {
+                label: "Create",
+                handler: () => Inertia.visit(route(`${__baseUrl}.create`)),
+                class: ["btn-primary"],
+            },
+        };
+        const __topActions = Object.values(
+            merge(defaultTopActions, props.topActions)
+        )
+            .filter((action) => action)
+            .filter((action) =>
+                action?.shouldRender ? action.shouldRender(props) : true
+            );
+        return { form, topActions: __topActions };
+    },
+});
 </script>
--- resources/js/Pages/Files/Show.vue
+++ resources/js/Pages/Files/Show.vue
@@ -3,121 +3,22 @@
         <template #header>
             <h2 class="font-semibold text-xl leading-tight">File Manager</h2>
         </template>
-        <jet-bar-container class="relative">
-            <div class="flex flex-row items-center mb-4">
-                <search-filter
-                    v-model:modelValue="form.search"
-                    class="w-full max-w-md mr-4"
-                    @reset="reset"
-                >
-                    <div class="block py-2 text-xs text-gray-400">Trashed:</div>
-                    <select
-                        v-model="form.trashed"
-                        class="mt-1 w-full form-select"
-                    >
-                        <option :value="null" />
-                        <option value="with">With Trashed</option>
-                        <option value="only">Only Trashed</option>
-                    </select>
-                </search-filter>
-                <div class="flex-grow" />
-                <inertia-link
-                    class="btn btn-primary justify-self-end"
-                    :href="route('files.upload')"
-                >
-                    <span>Upload</span>
-                </inertia-link>
-            </div>
             <gym-revenue-crud
                 modelName="file"
                 :fields="tableHeaders"
                 :resource="files"
                 titleField="filename"
                 :card-component="FileDataCard"
-                :actions="{edit: {label: 'Rename'}}"
+                :actions="{ edit: { label: 'Rename' } }"
+                :top-actions="{
+                    create: {
+                        label: 'Upload',
+                        handler: () => {
+                            Inertia.visit(route('files.upload'));
+                        },
+                    },
+                }"
             />
-            <!--            <gym-revenue-table :headers="tableHeaders" :resource="files">-->
-            <!--                <tr-->
-            <!--                    class="hover"-->
-            <!--                    v-for="file in files?.data"-->
-            <!--                    :key="file.id"-->
-            <!--                    @dblclick="selectedFile = file.deletedAt ? null : file"-->
-            <!--                >-->
-            <!--                    &lt;!&ndash;                    <td v-if="!isClientUser">{{ file.client.name }}</td>&ndash;&gt;-->
-            <!--                    <td>-->
-            <!--                        <div-->
-            <!--                            class="flex flex-row nowrap items-center gap-4 truncate"-->
-            <!--                        >-->
-            <!--                            <file-extension-icon-->
-            <!--                                :extension="file.extension"-->
-            <!--                                v-if="-->
-            <!--                                    ![-->
-            <!--                                        'jpg',-->
-            <!--                                        'jpeg',-->
-            <!--                                        'png',-->
-            <!--                                        'svg',-->
-            <!--                                        'webp',-->
-            <!--                                    ].includes(file.extension)-->
-            <!--                                "-->
-            <!--                                class="h-16 w-16"-->
-            <!--                            />-->
-            <!--                            <img-->
-            <!--                                :src="file.url"-->
-            <!--                                class="h-16 w-16 object-cover rounded-sm"-->
-            <!--                                v-else-->
-            <!--                            />-->
-            <!--                            <a-->
-            <!--                                :href="file.url"-->
-            <!--                                :download="file.filename"-->
-            <!--                                class="link link-hover"-->
-            <!--                                >{{ file.filename }}</a-->
-            <!--                            >-->
-            <!--                        </div>-->
-            <!--                    </td>-->
-            <!--                    <td>{{ new Date(file.created_at).toLocaleString() }}</td>-->
-            <!--                    <td>{{ prettyBytes(file.size) }}</td>-->
-            <!--                    <td class="flex flex-row justify-center space-x-2">-->
-            <!--                        &lt;!&ndash;                        <inertia-link :href="route('locations.trash', location.id)" class="text-gray-400 hover:text-gray-500">&ndash;&gt;-->
-            <!--                        &lt;!&ndash;@todo: We need to add a confirmation before deleting to avoid accidental deletes&ndash;&gt;-->
-            <!--                        <div-->
-            <!--                            class="flex flex-row nowrap justify-center items-center gap-4"-->
-            <!--                        >-->
-            <!--                            <button-->
-            <!--                                @click="selectedFile = file"-->
-            <!--                                class="text-gray-400 hover:text-gray-300"-->
-            <!--                                v-if="!file?.deleted_at"-->
-            <!--                            >-->
-            <!--                                <jet-bar-icon type="pencil" fill />-->
-            <!--                            </button>-->
-            <!--                            <button-->
-            <!--                                @click="-->
-            <!--                                    file?.deleted_at-->
-            <!--                                        ? $inertia.post(-->
-            <!--                                              route('files.restore', file.id)-->
-            <!--                                          )-->
-            <!--                                        : $inertia.delete(-->
-            <!--                                              route('files.trash', file.id)-->
-            <!--                                          )-->
-            <!--                                "-->
-            <!--                                class="text-gray-400 hover:text-gray-300"-->
-            <!--                            >-->
-            <!--                                <jet-bar-icon-->
-            <!--                                    :type="-->
-            <!--                                        file?.deleted_at ? 'untrash' : 'trash'-->
-            <!--                                    "-->
-            <!--                                    fill-->
-            <!--                                />-->
-            <!--                            </button>-->
-            <!--                        </div>-->
-            <!--                        &lt;!&ndash;                        </inertia-link>&ndash;&gt;-->
-            <!--                    </td>-->
-            <!--                </tr>-->
-
-            <!--                <tr v-if="!files?.data?.length">-->
-            <!--                    <td :colspan="tableHeaders.lenth">No Files found.</td>-->
-            <!--                </tr>-->
-            <!--            </gym-revenue-table>-->
-        </jet-bar-container>
         <sweet-modal
             title="Rename File"
             width="85%"
@@ -163,6 +64,7 @@ import mapValues from "lodash/mapValues";
 import SweetModal from "@/Components/SweetModal3/SweetModal";
 import FileDataCard from "./Partials/FileDataCard";
 import FilenameTableColumn from "./Partials/FilenameTableColumn";
+import { Inertia } from "@inertiajs/inertia";
 
 export default defineComponent({
     components: {
@@ -192,8 +94,20 @@ export default defineComponent({
             }
         });
 
-        const tableHeaders = [{label: "filename", component: FilenameTableColumn}, "size", "created_at", "updated_at"];
-        return { prettyBytes, modal, selectedFile, tableHeaders, FileDataCard };
+        const tableHeaders = [
+            { label: "filename", component: FilenameTableColumn },
+            "size",
+            "created_at",
+            "updated_at",
+        ];
+        return {
+            prettyBytes,
+            modal,
+            selectedFile,
+            tableHeaders,
+            FileDataCard,
+            Inertia,
+        };
     },
     watch: {
         form: {

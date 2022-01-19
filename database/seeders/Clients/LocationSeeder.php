ede56ab322621342e8fd0d4cdd0ddc623e778d71
LAPTOP-L96IKIQQ\drste <steve@topdawg.com>
2022-01-19 16:30:45 -0500

    added city and zip to loaction seeder

--- database/seeders/Clients/LocationSeeder.php
+++ database/seeders/Clients/LocationSeeder.php
@@ -20,36 +20,48 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Honolulu',
+                'zip' => '96795',
                 'location_no' => 'TZ01',
                 'gymrevenue_id' => 'TZ01'
             ],
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Honolulu',
+                'zip' => '96795',
                 'location_no' => 'TZ02',
                 'gymrevenue_id' => 'TZ02'
             ],
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Honolulu',
+                'zip' => '96796',
                 'location_no' => 'TZ03',
                 'gymrevenue_id' => 'TZ03'
             ],
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Honolulu',
+                'zip' => '96797',
                 'location_no' => 'TZ04',
                 'gymrevenue_id' => 'TZ04'
             ],
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Honolulu',
+                'zip' => '96798',
                 'location_no' => 'TZ05',
                 'gymrevenue_id' => 'TZ05'
             ],
             [
                 'client_id' => 'The Z',
                 'state' => 'HI',
+                'city' => 'Waimanalo',
+                'zip' => '96795',
                 'location_no' => 'TZ06',
                 'gymrevenue_id' => 'TZ06'
             ],
@@ -57,30 +69,40 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'Stencils',
                 'state' => 'CA',
+                'city' => 'Los Angeles',
+                'zip' => '90001',
                 'location_no' => 'ST01',
                 'gymrevenue_id' => 'ST07'
             ],
             [
                 'client_id' => 'Stencils',
                 'state' => 'CA',
+                'city' => 'San Diego',
+                'zip' => '22400',
                 'location_no' => 'ST02',
                 'gymrevenue_id' => 'ST08'
             ],
             [
                 'client_id' => 'Stencils',
                 'state' => 'CA',
+                'city' => 'San Jose',
+                'zip' => '94088',
                 'location_no' => 'ST03',
                 'gymrevenue_id' => 'ST09'
             ],
             [
                 'client_id' => 'Stencils',
                 'state' => 'OR',
+                'city' => 'Portland',
+                'zip' => '97035',
                 'location_no' => 'ST05',
                 'gymrevenue_id' => 'ST10'
             ],
             [
                 'client_id' => 'Stencils',
                 'state' => 'OR',
+                'city' => 'Portland',
+                'zip' => '97035',
                 'location_no' => 'ST05',
                 'gymrevenue_id' => 'ST11'
             ],
@@ -88,67 +110,88 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'The Kalamazoo',
                 'state' => 'LA',
+                'city' => 'Shreveport',
+                'zip' => '71101',
                 'location_no' => '001',
                 'gymrevenue_id' => 'TK12'
             ],
-
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abbott',
+                'zip' => '76621',
                 'location_no' => 'TR22',
                 'gymrevenue_id' => 'FT13'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abernathy',
+                'zip' => '79311',
                 'location_no' => 'TR32',
                 'gymrevenue_id' => 'FT14'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abram',
+                'zip' => '78572',
                 'location_no' => 'TR33',
                 'gymrevenue_id' => 'FT15'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abbott',
+                'zip' => '76621',
                 'location_no' => 'TR34',
                 'gymrevenue_id' => 'FT16'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abernathy',
+                'zip' => '79311',
                 'location_no' => 'TR55',
                 'gymrevenue_id' => 'FT17'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abbott',
+                'zip' => '76621',
                 'location_no' => 'TR36',
                 'gymrevenue_id' => 'FT18'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abbott',
+                'zip' => '76621',
                 'location_no' => 'TR32',
                 'gymrevenue_id' => 'FT19'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abernathy',
+                'zip' => '79311',
                 'location_no' => 'TR40',
                 'gymrevenue_id' => 'FT20'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TX',
+                'city' => 'Abbott',
+                'zip' => '76621',
                 'location_no' => 'TR65',
                 'gymrevenue_id' => 'FT21'
             ],
             [
                 'client_id' => 'FitnessTruth',
                 'state' => 'TN',
+                'city' => 'Adams',
+                'zip' => '37010',
                 'location_no' => 'TR70',
                 'gymrevenue_id' => 'FT22'
             ],
@@ -156,18 +199,24 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'Bodies By Brett',
                 'state' => 'FL',
+                'city' => 'Tampa',
+                'zip' => '33607',
                 'location_no' => '001',
                 'gymrevenue_id' => 'BB23'
             ],
             [
                 'client_id' => 'Bodies By Brett',
                 'state' => 'FL',
+                'city' => 'Tampa',
+                'zip' => '33607',
                 'location_no' => '002',
                 'gymrevenue_id' => 'BB24'
             ],
             [
                 'client_id' => 'Bodies By Brett',
                 'state' => 'FL',
+                'city' => 'Tampa',
+                'zip' => '33607',
                 'location_no' => '003',
                 'gymrevenue_id' => 'BB25'
             ],
@@ -175,24 +224,32 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'Sci-Fi Purple Gyms',
                 'state' => 'NC',
+                'city' => 'Advance',
+                'zip' => '27006',
                 'location_no' => 'PF01',
                 'gymrevenue_id' => 'PF26'
             ],
             [
                 'client_id' => 'Sci-Fi Purple Gyms',
                 'state' => 'NC',
+                'city' => 'Advance',
+                'zip' => '27006',
                 'location_no' => 'PF02',
                 'gymrevenue_id' => 'PF27'
             ],
             [
                 'client_id' => 'Sci-Fi Purple Gyms',
                 'state' => 'FL',
+                'city' => 'Ponte Vedra Beach',
+                'zip' => '32004',
                 'location_no' => 'PF03',
                 'gymrevenue_id' => 'PF28'
             ],
             [
                 'client_id' => 'Sci-Fi Purple Gyms',
                 'state' => 'FL',
+                'city' => 'Callahan',
+                'zip' => '32011',
                 'location_no' => 'PF04',
                 'gymrevenue_id' => 'PF29'
             ],
@@ -200,60 +257,80 @@ class LocationSeeder extends Seeder
             [
                 'client_id' => 'iFit',
                 'state' => 'FL',
+                'city' => 'Tampa',
+                'zip' => '33605',
                 'location_no' => 'I01',
                 'gymrevenue_id' => 'IF30'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'FL',
+                'city' => 'Lake City',
+                'zip' => '32025',
                 'location_no' => 'I02',
                 'gymrevenue_id' => 'IF31'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'FL',
+                'city' => 'Hilliard',
+                'zip' => '32046',
                 'location_no' => 'I03',
                 'gymrevenue_id' => 'IF32'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'FL',
+                'city' => 'Orange Park',
+                'zip' => '32065',
                 'location_no' => 'I04',
                 'gymrevenue_id' => 'IF33'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'FL',
+                'city' => 'Tampa',
+                'zip' => '33601',
                 'location_no' => 'I05',
                 'gymrevenue_id' => 'IF34'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'GA',
+                'city' => 'Atlanta',
+                'zip' => '30301',
                 'location_no' => 'I06',
                 'gymrevenue_id' => 'IF35'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'GA',
+                'city' => 'Atlanta',
+                'zip' => '30302',
                 'location_no' => 'I13',
                 'gymrevenue_id' => 'IF36'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'GA',
+                'city' => 'Atlanta',
+                'zip' => '30303',
                 'location_no' => 'I12',
                 'gymrevenue_id' => 'IF37'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'VA',
+                'city' => 'Virginia Beach',
+                'zip' => '23450',
                 'location_no' => 'I11',
                 'gymrevenue_id' => 'IF38'
             ],
             [
                 'client_id' => 'iFit',
                 'state' => 'VA',
+                'city' => 'Virginia Beach',
+                'zip' => '23451',
                 'location_no' => 'I10',
                 'gymrevenue_id' => 'IF39'
             ],

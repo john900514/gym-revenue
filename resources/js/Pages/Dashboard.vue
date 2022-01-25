37cfb3a3544df62d4b463066a04b98e40c06b917
Angel Gonzalez <angel@capeandbay.com>
2022-01-25 18:22:27 -0500

    Updating the dashboard with a deployment announement bubble above the dashboard when there's a record in app_states that's less than 5 days old, show the section if the active team, is a cape and bay team. It will need to live in its own component when scalling the announcements feature. Added Seeder with logic to add deployment announcements.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,56 @@
 <template>
     <app-layout>
         <template #header>
-            <div class="flex flex-row justify-between">
+            <!-- @todo - move this to its own component when announcements scale out -->
+            <div class="flex flex-row justify-center pb-4" v-if="(announcements.length > 0) && showAnnouncement">
+                <div class="alert alert-info xl:grid-cols-4">
+                <div class="flex-1 flex-col">
+                    <div class="flex flex-row">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                        <label>New Version deployed! - v{{ announcements[0].version }}</label>
+                    </div>
+
+                    <br />
+                    <ul>
+                        <li v-for="(note, x) in announcements[0].notes" v-show="x < 3"> * {{ note }}</li>
+                    </ul>
+
+                    <div class="flex flex-row pt-6 ml-4">
+                        <button type="button" @click="announcementModal = true" class="pr-4 text-white hover:text-info"><label class="uppercase" style="letter-spacing: 0.1em;font-weight: 600;cursor:pointer;">See More</label></button>
+                        <button type="button" @click="showAnnouncement = false" class="pr-4 text-white hover:text-error"><label class="uppercase" style="letter-spacing: 0.1em;font-weight: 600;cursor:pointer;">Close Me</label></button>
+                    </div>
+                </div>
+                <announce-modal title="Deployment Announcement"
+                                width="85%"
+                                overlayTheme="dark"
+                                modal-theme="dark"
+                                hideCloseButton
+                                ref="anModal"
+                                @confirm="announcementModal = false"
+                                @cancel="announcementModal = false"
+                >
+                    <div class="flex-1 flex-col">
+                        <div class="flex flex-row">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <label>New Version deployed! - v{{ announcements[0].version }}</label>
+                        </div>
+
+                        <br />
+                        <ul>
+                            <li v-for="(note, x) in announcements[0].notes"> * {{ note }}</li>
+                        </ul>
+                        <div class="flex flex-row pt-6 ml-4">
+                            <button type="button" @click="announcementModal = false" class="btn btn-success pr-4 text-white hover:text-error"><label class="uppercase" style="letter-spacing: 0.1em;font-weight: 600;cursor:pointer;">Okay!</label></button>
+                        </div>
+                    </div>
+                </announce-modal>
+            </div>
+            </div>
+            <div class="flex flex-row justify-between" >
                 <div><p>Dashboard</p></div>
                 <div><p>{{ accountName }}</p></div>
             </div>
@@ -73,6 +122,7 @@ import JetBarStatCard from "@/Components/JetBarStatCard";
 import GymRevenueTable from "@/Components/CRUD/GymRevenueTable";
 import JetBarBadge from "@/Components/JetBarBadge";
 import JetBarIcon from "@/Components/JetBarIcon";
+import AnnounceModal from "@/Components/SweetModal3/SweetModal";
 
 export default {
     components: {
@@ -84,16 +134,36 @@ export default {
         GymRevenueTable,
         JetBarBadge,
         JetBarIcon,
+        AnnounceModal
     },
     props: [
         'clients',
         'accountName',
-        'widgets'
+        'widgets',
+        'announcements'
     ],
-    data() {},
+    watch: {
+        announcementModal(flag) {
+            if(flag) {
+                this.$refs.anModal.open();
+            }
+            else {
+                this.$refs.anModal.close();
+            }
+        }
+    },
+    data() {
+        return {
+            showAnnouncement: false,
+            announcementModal: false
+        }
+    },
     methods: {},
     computed: {},
     mounted() {
+        if (this.announcements.length > 0) {
+            this.showAnnouncement = true;
+        }
         console.log('GymRevenue Dashboard');
     }
 }

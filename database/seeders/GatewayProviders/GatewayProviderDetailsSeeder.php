ea55a689e32ccde44c34c92d8abc954be2d8f0c5
Steven Deemer <steve@capeandbay.com>
2022-02-21 17:20:58 -0500

    Provider details seeder for default C&B

--- database/seeders/GatewayProviders/GatewayProviderDetailsSeeder.php
+++ database/seeders/GatewayProviders/GatewayProviderDetailsSeeder.php
@@ -23,21 +23,21 @@ class GatewayProviderDetailsSeeder extends Seeder
                     'gateway_id' => $gateways['twilio']['id'],
                     'detail' => 'access_credential',
                     'value' => 'twilio_sid',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => 'AC736b5b35b4217cb32e3d907c3a5a8914'],
                     'active' => 1
                 ],
                 [
                     'gateway_id' => $gateways['twilio']['id'],
                     'detail' => 'access_credential',
                     'value' => 'twilio_token',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => 'b700f3dbecb34f37c994263b250a98e7'],
                     'active' => 1
                 ],
                 [
                     'gateway_id' => $gateways['twilio']['id'],
                     'detail' => 'access_credential',
                     'value' => 'twilio_no',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => '+19106345244'],
                     'active' => 1
                 ],
             ],
@@ -46,21 +46,21 @@ class GatewayProviderDetailsSeeder extends Seeder
                     'gateway_id' => $gateways['mailgun']['id'],
                     'detail' => 'access_credential',
                     'value' => 'mailgun_domain',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => 'auto.theathleticclub.com'],
                     'active' => 1
                 ],
                 [
                     'gateway_id' => $gateways['mailgun']['id'],
                     'detail' => 'access_credential',
                     'value' => 'mailgun_secret',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => '450a0212c7352bcc84d8feb5b05205d2-2b0eef4c-99b3599c'],
                     'active' => 1
                 ],
                 [
                     'gateway_id' => $gateways['mailgun']['id'],
                     'detail' => 'access_credential',
                     'value' => 'mailgun_endpoint',
-                    'misc' => ['required' => true],
+                    'misc' => ['required' => true,'value' => 'api.mailgun.net'],
                     'active' => 1
                 ],
                 [

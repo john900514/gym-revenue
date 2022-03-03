409a7d302720532eb2364b6d3ee49cf4d3bb7a0a
blair <blair@capeandbay.com>
2022-03-03 14:27:16 -0500

    Created migration and model

---
+++ app/Models/Note.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Note extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = ['id', 'entity_id', 'entity_type', 'note', 'created_by_user_id', 'active'];
+
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('note', 'like', '%' . $search . '%');
+            });
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
+}

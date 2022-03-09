b094766af63605f3e5035fe9e1a11b8d06a1117a
Angel Gonzalez <angel@capeandbay.com>
2022-03-09 16:21:18 -0500

    Support for API Token viewing. Restoring adding Account Owner to default team to fix a Dashboard/Toolbar bug.

---
+++ resources/js/Pages/Profile/Partials/ShowAPITokenForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <jet-form-section>
+        <template #title>
+            API Access
+        </template>
+
+        <template #description>
+            View your Access Token! It will bo in the Authorization header with a Bearer in front of it.
+        </template>
+
+        <template #form>
+            <div class="col-span-6 sm:col-span-6">
+                <jet-label for="mockup-code" value="In the Request Header" />
+                <div class="mockup-code border border-secondary mt-4">
+                    <pre data-prefix=""><code>Authorization: Bearer {{ token }}</code></pre>
+                </div>
+            </div>
+        </template>
+    </jet-form-section>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import JetActionMessage from '@/Jetstream/ActionMessage'
+import Button from '@/Components/Button'
+import JetFormSection from '@/Jetstream/FormSection'
+
+import JetInputError from '@/Jetstream/InputError'
+import JetLabel from '@/Jetstream/Label'
+
+export default {
+    name: "ShowAPITokenForm",
+    components: {
+        JetActionMessage,
+        Button,
+        JetFormSection,
+
+        JetInputError,
+        JetLabel,
+    },
+    props: ['token']
+}
+</script>
+
+<style scoped>
+
+</style>

be81c0c838b2443550d46afea5631307a2cec746
Sterling Webb <sterling@capeandbay.com>
2022-04-05 12:01:26 -0400

    default to all fields until we come up with a default list

--- resources/js/Components/CRUD/helpers/getCustomizedFields.js
+++ resources/js/Components/CRUD/helpers/getCustomizedFields.js
@@ -4,6 +4,6 @@ import {getCrudConfig} from "@/utils/getCrudCustomization";
 export const getCustomizedFields = (fields, modelKey) => {
     //now filter out fields we don't care about
     const config = getCrudConfig(modelKey);
-    const filtered = computed(() => fields.value.filter(field => config.value.find(f => f === field.name)));
+    const filtered = computed(() => config.value?.length ? fields.value.filter(field => config.value.find(f => f === field.name)) : fields.value);
     return filtered;
 };

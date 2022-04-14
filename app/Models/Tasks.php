d82e181074c43546faaaa86295032027ab7cb960
Steven Deemer <steve@capeandbay.com>
2022-04-14 15:31:12 -0400

    Renamed from tasks to Tasks

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class tasks extends Model
+class Tasks extends Model
 {
     use HasFactory, SoftDeletes, Uuid;
 

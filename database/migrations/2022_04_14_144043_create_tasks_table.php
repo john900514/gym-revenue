ecd84523129560f53afc066137d8d04f0b45c8e7
Steven Deemer <steve@capeandbay.com>
2022-04-14 10:57:04 -0400

    migration of new table tasks completed

---
+++ database/migrations/2022_04_14_144043_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->unique()->primary();
+            $table->foreignUuid('user_id');
+            $table->dateTime('due_at')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}

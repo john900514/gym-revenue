96e715132eafee4fe57b7a9c7071247abd518f5c
Steven Deemer <steve@capeandbay.com>
2022-04-15 13:28:33 -0400

    stuff

--- app/Actions/Clients/Tasks/CreateTask.php
+++ app/Actions/Clients/Tasks/CreateTask.php
@@ -6,10 +6,8 @@ namespace App\Actions\Clients\Tasks;
 use App\Aggregates\Users\UserAggregate;
 
 use App\Helpers\Uuid;
-//use App\Models\Calendar\CalendarEvent;
-//use App\Models\Calendar\CalendarEventType;
-//use App\Models\Endusers\Lead;
 use App\Models\User;
+use App\StorableEvents\Clients\Tasks\TaskCreated;
 use Illuminate\Support\Facades\Redirect;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -27,16 +25,6 @@ class CreateTask
      */
     public function rules()
     {
-/*
- *
-user_id: id
-due_at: timestamp - nullable
-completed_at - timestamp - nullable
-timestamps
-softdeletes
- *
- */
-
         return [
             'title' =>['required', 'string','max:50'],
             'description' => ['string', 'nullable'],
@@ -52,49 +40,30 @@ softdeletes
         $data['id'] = $id;
 
 
-        $data['color'] = CalendarEventType::whereId($data['event_type_id'])->first()->color;; //Pulling eventType color for this table because that's how fullCalender.IO wants it
 
         if(isset($user->id))
-            $data['user_attendees'][] = $user->id; //If you make the event, you're automatically an attendee.
+//            $data['user_attendees'][] = $user->id; //If you make the event, you're automatically an attendee.
 
-        if(!is_null($data['user_attendees'])) {
-            $data['user_attendees'] = array_values(array_unique($data['user_attendees'])); //This will dupe check and then re-index the array.
-            foreach($data['user_attendees'] as $user) {
+        if(!is_null($data['title'])) {
                 $user = User::whereId($user)->select('id', 'name', 'email')->first();
                 if($user) {
-                    CalendarAggregate::retrieve($data['client_id'])
-                        ->addCalendarAttendee($user->id ?? "Auto Generated",
+                    UserAggregate::retrieve($data['client_id'])
+                        ->addTaskCreated($user->id ?? "Auto Generated",
                             [
                         'entity_type' => User::class,
-                        'entity_id' => $user->id,
-                        'entity_data' => $user,
-                        'calendar_event_id' => $id,
-                        'invitation_status' => 'Invitation Pending'
+                        'user_id' => $user->id,
+                        'due_at' => $user,
+                        'title' => $id,
+                        'description' => 'Invitation Pending'
                         ])->persist();
                 }
             }
         }
 
-        if(!empty($data['lead_attendees'])) {
-            $data['lead_attendees'] = array_values(array_unique($data['lead_attendees'])); //This will dupe check and then re-index the array.
-            foreach($data['lead_attendees'] as $lead) {
-                $lead = Lead::whereId($lead)->select('id', 'first_name', 'last_name', 'email')->first();
-                if($lead) {
-                    CalendarAggregate::retrieve($data['client_id'])
-                        ->addCalendarAttendee($user->id ?? "Auto Generated",
-                            [
-                                'entity_type' => Lead::class,
-                                'entity_id' => $lead->id,
-                                'entity_data' => $lead,
-                                'calendar_event_id' => $data['id'],
-                                'invitation_status' => 'Invitation Pending'
-                            ])->persist();
-                }
-            }
-        }
 
-        unset($data['user_attendees']);
-        unset($data['lead_attendees']);
+
+ //       unset($data['user_attendees']);
+ //       unset($data['lead_attendees']);
 
 
         UserAggregate::retrieve($data['client_id'])
@@ -107,7 +76,7 @@ softdeletes
     public function authorize(ActionRequest $request): bool
     {
         $current_user = $request->user();
-        return $current_user->can('calendar.create', CalendarEvent::class);
+        return $current_user->can('task.create', TaskCreated::class);
     }
 
     public function asController(ActionRequest $request)

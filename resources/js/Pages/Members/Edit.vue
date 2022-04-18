aaa06cc08273199ca7231ee5ab0ba830bb8fe79d
Sterling Webb <sterling@capeandbay.com>
2022-04-18 10:05:46 -0400

    ui

---
+++ resources/js/Pages/Members/Edit.vue
@@ -0,0 +1,55 @@
+<template>
+    <app-layout title="Edit Member">
+        <template #header>
+            <jet-bar-icon type="g0back" fill/>
+            <h2 class="font-semibold text-xl  leading-tight">
+                Edit Member
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <member-form
+                    :user-id="user_id"
+                    :client-id="this.$page.props.user.current_client_id"
+                    :member="member"
+                    :locations="locations"
+                    :available_services="available_services"
+                    :interaction-count="interactionCount"/>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout'
+import Button from '@/Components/Button'
+import JetFormSection from '@/Jetstream/FormSection'
+
+import JetInputError from '@/Jetstream/InputError'
+import JetLabel from '@/Jetstream/Label'
+import JetBarIcon from '@/Components/JetBarIcon'
+
+import MemberForm from '@/Pages/Members/Partials/MemberForm'
+import {defineComponent} from 'vue'
+
+
+export default defineComponent({
+    components: {
+        AppLayout,
+        Button,
+        JetFormSection,
+        JetInputError,
+        JetLabel,
+        JetBarIcon,
+        MemberForm,
+    },
+    props: [
+        'user_id',
+        'member',
+        'locations',
+        'available_services',
+        'interactionCount',
+    ]
+})
+</script>
---
+++ resources/js/Pages/Members/Index.vue
@@ -0,0 +1,181 @@
+<template>
+    <app-layout :title="title">
+        <page-toolbar-nav title="Member" :links="navLinks" />
+        <gym-revenue-crud
+            :resource="members"
+            model-key="member"
+            :fields="fields"
+            :base-route="baseRoute"
+            :top-actions="{
+                create: { label: 'Add Member' },
+            }"
+            :actions="actions"
+            :preview-component="MemberPreview"
+        >
+            <template #filter>
+                <leads-filters :base-route="baseRoute" />
+            </template>
+        </gym-revenue-crud>
+        <confirm
+            title="Really Trash?"
+            v-if="confirmTrash"
+            @confirm="handleConfirmTrash"
+            @cancel="confirmTrash = null"
+        >
+            {{ firstName }} {{ lastName }} Are you sure you want to remove this
+            lead?<br />
+            Reason for Deleting:<br />
+            <select name="reasonforremoving" v-model="trashReason">
+                <option>Select a reason</option>
+                <option value="duplicate">Is a duplicate</option>
+                <option value="test-lead">Is a test lead</option>
+                <option value="DNC">Lead requested DNC and data removal</option>
+                <option value="person-non-existing">
+                    This person does not exist
+                </option>
+                <option value="mistake-creating">
+                    I made a mistake creating this lead
+                </option>
+                <option value="other">Other</option>
+            </select>
+        </confirm>
+    </app-layout>
+</template>
+
+<script>
+import { computed, defineComponent, ref } from "vue";
+import { Inertia } from "@inertiajs/inertia";
+import AppLayout from "@/Layouts/AppLayout";
+import Confirm from "@/Components/Confirm";
+
+import Button from "@/Components/Button";
+import JetBarContainer from "@/Components/JetBarContainer";
+import GymRevenueCrud from "@/Components/CRUD/GymRevenueCrud";
+// import LeadInteraction from "./Partials/LeadInteractionContainer";
+import CrudBadge from "@/Components/CRUD/Fields/CrudBadge";
+import PageToolbarNav from "@/Components/PageToolbarNav";
+import MemberFilters from "@/Pages/Members/Partials/MemberFilters";
+import MemberPreview from "@/Pages/Members/Partials/MemberPreview";
+
+export default defineComponent({
+    components: {
+        MemberFilters,
+        PageToolbarNav,
+        GymRevenueCrud,
+        AppLayout,
+        Confirm,
+        Button,
+        JetBarContainer,
+        // LeadInteraction,
+        MemberPreview,
+    },
+    props: [
+        "members",
+        "routeName",
+        "title",
+        "filters",
+        "grlocations",
+        "user",
+        "nameSearch",
+    ],
+    setup(props) {
+        const comingSoon = () => {
+            new Noty({
+                type: "warning",
+                theme: "sunset",
+                text: "Feature Coming Soon!",
+                timeout: 7500,
+            }).show();
+        };
+
+        const fields = [
+            { name: "first_name", label: "First Name" },
+            { name: "last_name", label: "Last Name" },
+            { name: "location.name", label: "Location" },
+            { name: "created_at", label: "Joined" },
+            { name: "updated_at", label: "Updated" },
+        ];
+
+        const actions = {
+            trash: {
+                handler: ({ data }) => handleClickTrash(data.id),
+            },
+
+            contact: {
+                label: "Contact Member",
+                handler: ({ data }) => {
+                    Inertia.visit(route("data.members.show", data.id));
+                },
+            },
+        };
+        const trashReason = ref(null);
+
+        const confirmTrash = ref(null);
+        const handleClickTrash = (id) => {
+            confirmTrash.value = id;
+        };
+        handleClickTrash();
+        const handleConfirmTrash = () => {
+            Inertia.delete(route("data.members.trash", confirmTrash.value), {
+                data: { reason: trashReason.value },
+            });
+            confirmTrash.value = null;
+        };
+        const baseRoute = "data.members";
+        const navLinks = [
+            {
+                label: "Dashboard",
+                href: "#",
+                onClick: comingSoon,
+                active: false,
+            },
+            {
+                label: "CalendarEvent",
+                href: "#",
+                onClick: comingSoon,
+                active: false,
+            },
+            {
+                label: "Members",
+                href: "#",
+                onClick: comingSoon,
+                active: true,
+            },
+            {
+                label: "Tasks",
+                href: "#",
+                onClick: comingSoon,
+                active: false,
+            },
+            {
+                label: "Contacts",
+                href: "#",
+                onClick: comingSoon,
+                active: false,
+            },
+            {
+                label: "Consultants",
+                href: "#",
+                onClick: comingSoon,
+                active: false,
+            },
+        ];
+
+        return {
+            handleClickTrash,
+            confirmTrash,
+            handleConfirmTrash,
+            fields,
+            actions,
+            Inertia,
+            comingSoon,
+            navLinks,
+            baseRoute,
+            MemberPreview,
+            trashReason,
+        };
+    },
+});
+</script>
+
+<style scoped></style>

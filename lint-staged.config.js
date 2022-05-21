e5ee3b566eeef9cc0736f0ed140b91ac516bde2d
philip <philip@capeandbay.com>
2022-05-21 17:58:10 -0400

    add in huskier for pre-commit hooks. add lint-staged in order to only run formatters on files that have been changed. add prettier to format JS/Vue, and php-cs-fixer for php

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'resources/**/*.{css,js,svelte}': ['prettier --write'],
+    'resources/**/*.{css,js,vue}': ['prettier --write'],
     '**/*.php': ['php ./vendor/bin/php-cs-fixer fix --config .php-cs-fixer.php'],
 };

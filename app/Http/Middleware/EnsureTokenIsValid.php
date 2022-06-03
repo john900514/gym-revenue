d9aca961b04fdafddd5c8a81bf92a9dbcca6349f
blair <blair@capeandbay.com>
2022-06-03 18:39:09 -0400

    Modifying philip's code to look better bc thats why i have a job.

--- app/Http/Middleware/EnsureTokenIsValid.php
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Clients\Client;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
@@ -18,24 +19,22 @@ class EnsureTokenIsValid
      */
     public function handle(Request $request, Closure $next)
     {
-        $token = '';
-
         $header = $request->header('Authorization', '');
         if (Str::startsWith($header, 'Bearer ')) {
-            $token = Str::substr($header, 7);
-        }
+            $token = Str::substr($header, 7); // Splice token out of header
+            $user = User::whereAccessToken($token)
+                ->firstOrFail(); //Find user, if it fails, it will 404.
 
-        if ($token == '') {
+            if ($user->isClientUser()) {
+                $request->merge(['client_id' => $user->client_id]);
+            } elseif ($user->isCapeAndBayUser()) {
+                $client_id = $request->header('Client');
+                Client::whereId($client_id)
+                    ->firstOrfail(); //Find client, if it fails, it will 404.
+                $request->merge(['client_id' => $client_id]); //Merge client_id into request.
+            }
+        } else {
             abort(403);
-        }
-
-        $user = User::whereAccessToken($token)->firstOrFail();
-
-        if ($user->isClientUser()) {
-            $request->merge(['client_id' => $user->client_id]);
-        } elseif ($user->isCapeAndBayUser()) {
-            $client_id = $request->header('Client');
-            $request->merge(['client_id' => $client_id]);
         }
 
         return $next($request);

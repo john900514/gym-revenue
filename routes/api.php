5ad84e0ddbdd33238a6ea548ed512ea30b95dfb4
blair <blair@capeandbay.com>
2022-06-04 08:30:43 -0400

    Successfully created member and lead

--- routes/api.php
+++ routes/api.php
@@ -21,15 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware(EnsureTokenIsValid::class)->prefix('leads')->group(function () {
     Route::get('/', \App\Actions\Endusers\Leads\ReadLeads::class);
-    Route::post('/', \App\Actions\Endusers\Leads\CreateLead::class);
+    Route::post('/', \App\Actions\Endusers\Leads\CreateLeadApi::class);
 });
 
 Route::middleware(EnsureTokenIsValid::class)->prefix('members')->group(function () {
     Route::get('/', \App\Actions\Endusers\Members\ReadMembers::class);
-    Route::post('/', \App\Actions\Endusers\Members\CreateMember::class);
+    Route::post('/', \App\Actions\Endusers\Members\CreateMemberApi::class);
 });
-
-/*
-Route::middleware('auth:sanctum')->prefix('/members')->group(function () {
-    Route::get('/', \App\Actions\Endusers\Members\ReadMembers::class)->name('members.read');
-});*/

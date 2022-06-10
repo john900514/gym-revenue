f1c7e1ed14af29af7500839ade70168c6416605d
David Duncan <devduncan89@gmail.com>
2022-06-10 16:36:36 -0400

    remove sass

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" href="{{ mix('sass/app.css') }}">
+
 
         <!-- Scripts -->
         @routes

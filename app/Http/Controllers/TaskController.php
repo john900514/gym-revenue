b8104f75605b4a0beaa015a6c1aefc0bef066fbe
blair <blair@capeandbay.com>
2022-06-16 10:18:22 -0400

    Giving the tasks controller some TLC

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Calendar\CalendarEvent;
 use App\Models\Calendar\CalendarEventType;
+use App\Models\Clients\Client;
 use App\Models\Endusers\Lead;
 use App\Models\Reminder;
+use App\Models\TeamUser;
 use App\Models\User;
 use DateTime;
 use Illuminate\Http\Request;
@@ -89,11 +91,32 @@ class TaskController extends Controller
             $tasks[$key]->event_owner = User::whereId($event['owner_id'])->first() ?? null;
         }
 
+        if ($client_id) {
+            $current_team = $request->user()->currentTeam()->first();
+            $client = Client::whereId($client_id)->with('default_team_name')->first();
+
+            $is_default_team = $client->default_team_name->value == $current_team->id;
+
+            // If the active team is a client's-default team get all members
+            if ($is_default_team) {
+                $users = User::whereClientId($client_id)->get();
+            } else {
+                // else - get the members of that team
+                $team_users = TeamUser::whereTeamId($current_team->id)->get();
+                $user_ids = [];
+                foreach ($team_users as $team_user) {
+                    $user_ids[] = $team_user->user_id;
+                }
+                $users = User::whereIn('id', $user_ids)
+                    ->get();
+            }
+        }
+
         return Inertia::render('Task/Show', [
             'tasks' => $tasks,
             'client_id' => $client_id,
-            'client_users' => [],
-            'lead_users' => [],
+            'client_users' => $users,
+            'lead_users' => Lead::whereClientId($client_id)->select('id', 'first_name', 'last_name')->get(),
             'calendar_event_types' => CalendarEventType::whereClientId($client_id)->get(),
             'filters' => $request->all('search', 'trashed', 'state'),
             'incomplete_tasks' => $incomplete_tasks,

f76533d34da877cbceb115d8023237baa93ea510
David Duncan <devduncan89@gmail.com>
2022-06-30 09:37:15 -0400

    quick view

---
+++ resources/js/Pages/Calendar/QuickView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>test</h1>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+    calendar_events_by_locations: {
+        type: Array,
+        default: [],
+    },
+});
+
+console.log("calendar eventsss", props.calendar_events_by_locations);
+</script>

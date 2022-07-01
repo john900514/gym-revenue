24a058e57013a92b24e2a84431b6a2d1fb851419
David Duncan <devduncan89@gmail.com>
2022-07-01 01:06:40 -0400

    style/layout

--- resources/js/Pages/Calendar/components/LocationContainer.vue
+++ resources/js/Pages/Calendar/components/LocationContainer.vue
@@ -10,14 +10,43 @@
         </div>
         <ul>
             <li v-for="e in events" class="flex items-center my-4">
-                time
-                <div class="w-full bg-base-content px-3 py-1 ml-2 rounded">
-                    <span>{{ e.title }}</span>
+                {{ fmtTime(e.start) }}
+                <div
+                    class="w-full bg-base-content px-3 py-1 ml-2 rounded gridgrid-rows-1"
+                >
+                    <span
+                        class="whitespace-nowrap overflow-hidden overflow-ellipsis"
+                        >{{ e.title }}</span
+                    >
+                    <svg
+                        class="inline-block flex-shrink-0"
+                        width="24"
+                        height="24"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <circle
+                            cx="12"
+                            cy="12"
+                            r="11"
+                            :stroke="e.color"
+                            :fill="e.color"
+                            stroke-width="1"
+                        />
+                    </svg>
                 </div>
             </li>
         </ul>
     </article>
 </template>
+
+<style scoped>
+li > div {
+    @apply grid;
+    grid-template-columns: 1fr auto;
+    grid-template-rows: 1fr;
+}
+</style>
 
 <script>
 import { defineComponent, ref, onMounted } from "vue";
@@ -39,7 +68,14 @@ export default defineComponent({
         },
     },
     setup(props) {
-        return {};
+        const fmtTime = (d) => {
+            let t = d.split(" ")[1].split(":");
+            return [t[0], t[1]].join(":");
+        };
+
+        return {
+            fmtTime,
+        };
     },
 });
 </script>

d8776eec7f1d0dc5b2dbaf03c6056e374149646f
blair <blair@capeandbay.com>
2022-07-26 14:48:09 -0400

    fixing an import

--- app/Domain/Folders/Events/FolderDeleted.php
+++ app/Domain/Folders/Events/FolderDeleted.php
@@ -7,7 +7,7 @@ use App\StorableEvents\EntityDeleted;
 
 class FolderDeleted extends EntityDeleted
 {
-    protected function getEntity(): string
+    public function getEntity(): string
     {
         return Folder::class;
     }

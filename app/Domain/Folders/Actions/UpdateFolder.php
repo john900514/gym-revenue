dd53e3d1e69cfeac973a30c469c0633dc1552afd
blair <blair@capeandbay.com>
2022-08-16 12:06:09 -0400

    Adding folder name update

---
+++ app/Domain/Folders/Actions/UpdateFolder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Domain\Folders\Actions;
+
+use App\Domain\Folders\FolderAggregate;
+use App\Http\Middleware\InjectClientId;
+use App\Models\Folder;
+use App\Support\Uuid;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use Laravel\Jetstream\Contracts\CreatesTeams;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Prologue\Alerts\Facades\Alert;
+
+class UpdateFolder implements CreatesTeams
+{
+    use AsAction;
+
+    public function handle(array $payload): Folder
+    {
+        $id = Uuid::new();
+
+        FolderAggregate::retrieve($id)->update($payload)->persist();
+
+        return Folder::findOrFail($id);
+    }
+
+    /**
+     * Get the validation rules that apply to the action.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'client_id' => ['sometimes', 'nullable','string', 'max:255', 'exists:clients,id'],
+            'name' => ['required', 'max:50'],
+        ];
+    }
+
+    public function getControllerMiddleware(): array
+    {
+        return [InjectClientId::class];
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $current_user = $request->user();
+
+        return $current_user->can('folders.update', Folder::class);
+    }
+
+    public function asController(ActionRequest $request): Folder
+    {
+        return $this->handle(
+            $request->validated()
+        );
+    }
+
+    public function htmlResponse(Folder $folder): RedirectResponse
+    {
+        Alert::success("Folder '{$folder->name}' was updated")->flash();
+
+        //return Redirect::route('folder.edit', $folder->id);
+        return Redirect::back();
+    }
+
+    public function create($user, array $input): Folder
+    {
+        return $this->handle($input);
+    }
+}
--- app/Domain/Folders/FolderAggregate.php
+++ app/Domain/Folders/FolderAggregate.php
@@ -4,6 +4,7 @@ namespace App\Domain\Folders;
 
 use App\Domain\Folders\Events\FolderCreated;
 use App\Domain\Folders\Events\FolderDeleted;
+use App\Domain\Folders\Events\FolderUpdated;
 use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 
 class FolderAggregate extends AggregateRoot
@@ -11,6 +12,13 @@ class FolderAggregate extends AggregateRoot
     public function create(array $payload): static
     {
         $this->recordThat(new FolderCreated($payload));
+
+        return $this;
+    }
+
+    public function update(array $payload): static
+    {
+        $this->recordThat(new FolderUpdated($payload));
 
         return $this;
     }

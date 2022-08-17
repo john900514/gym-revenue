198a46d17c5ce54c71fae8a702e6c501118e23f6
blair <blair@capeandbay.com>
2022-08-17 13:34:09 -0400

    fixing search

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -20,32 +20,62 @@ class FilesController extends Controller
             return Redirect::route('dashboard');
         }
 
+        $isSearching = $request->has('search');
+
         $page_count = 10;
         $roles = request()->user()->getRoles();
         $security_group = request()->user()->securityGroup();
 
-        if ($security_group === SecurityGroupEnum::ADMIN || $security_group === SecurityGroupEnum::ACCOUNT_OWNER) {
-            $files = File::with('client')
-                ->whereClientId($client_id)
-                ->whereUserId(null)
-                ->whereHidden(false)
-                ->whereEntityType(null)
-                ->filter($request->only('search', 'trashed'))
-                ->sort()
-                ->paginate($page_count)
-                ->appends(request()->except('page'));
+        if ($isSearching) {
+            if ($security_group === SecurityGroupEnum::ADMIN || $security_group === SecurityGroupEnum::ACCOUNT_OWNER) {
+                $files = File::with('client')
+                    ->whereClientId($client_id)
+                    ->whereUserId(null)
+                    ->whereHidden(false)
+                    ->whereEntityType(null)
+                    ->filter($request->only('search', 'trashed'))
+                    ->sort()
+                    ->paginate($page_count)
+                    ->appends(request()->except('page'));
+            } else {
+                $files = File::with('client')
+                    ->whereClientId($client_id)
+                    ->whereUserId(null)
+                    ->whereHidden(false)
+                    ->whereEntityType(null)
+                    ->where('permissions', 'like', '%'.strtolower(str_replace(' ', '_', $roles[0])).'%')
+                    ->filter($request->only('search', 'trashed'))
+                    ->sort()
+                    ->paginate($page_count)
+                    ->appends(request()->except('page'));
+            }
         } else {
-            $files = File::with('client')
-                ->whereClientId($client_id)
-                ->whereUserId(null)
-                ->whereHidden(false)
-                ->whereEntityType(null)
-                ->where('permissions', 'like', '%'.strtolower(str_replace(' ', '_', $roles[0])).'%')
-                ->filter($request->only('search', 'trashed'))
-                ->sort()
-                ->paginate($page_count)
-                ->appends(request()->except('page'));
+            if ($security_group === SecurityGroupEnum::ADMIN || $security_group === SecurityGroupEnum::ACCOUNT_OWNER) {
+                $files = File::with('client')
+                    ->whereClientId($client_id)
+                    ->whereUserId(null)
+                    ->whereHidden(false)
+                    ->whereFolder(null)
+                    ->whereEntityType(null)
+                    ->filter($request->only('search', 'trashed'))
+                    ->sort()
+                    ->paginate($page_count)
+                    ->appends(request()->except('page'));
+            } else {
+                $files = File::with('client')
+                    ->whereClientId($client_id)
+                    ->whereUserId(null)
+                    ->whereHidden(false)
+                    ->whereFolder(null)
+                    ->whereEntityType(null)
+                    ->where('permissions', 'like', '%'.strtolower(str_replace(' ', '_', $roles[0])).'%')
+                    ->filter($request->only('search', 'trashed'))
+                    ->sort()
+                    ->paginate($page_count)
+                    ->appends(request()->except('page'));
+            }
         }
+
 
         $folders = Folder::with('files')
             ->get();

1897298c5f8d1a082a1f339e3b37bb8b2fea70eb
Philip Krogel <philip@capeandbay.com>
2022-08-24 23:16:08 -0400

    try resolving path different for seeding in vapor deploy

--- database/seeders/Clients/LocationSeeder.php
+++ database/seeders/Clients/LocationSeeder.php
@@ -384,7 +384,7 @@ class LocationSeeder extends Seeder
         ///now do trufit csv import
         VarDumper::dump("Adding TruFit Locations from CSV");
         $key = 'tmp/trufit-clubs';
-        $csv = file_get_contents('database/data/trufit-clubs.csv');
+        $csv = file_get_contents(realpath(__DIR__."/../../../database/data/trufit-clubs.csv"));
         Storage::disk('s3')->put($key, $csv);
         ImportLocations::run([
             [

25a17c785e8198d51ebbd919c00665dbb8cff68c
philip <philip@capeandbay.com>
2022-12-09 08:47:11 -0500

    remove unused user code after the merge w/ develoip

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,6 @@ use App\Domain\Locations\Projections\Location;
 use App\Domain\Teams\Models\Team;
 use App\Domain\Teams\Models\TeamUser;
 use App\Domain\Users\Models\User;
-use App\Domain\Users\Models\UserDetails;
 use App\Models\Position;
 use App\Models\ReadReceipt;
 use Illuminate\Http\Request;
@@ -37,68 +36,8 @@ class UsersController extends Controller
             $locations = Location::all();
             $teams = Team::findMany(Client::with('teams')->find($client_id)->teams->pluck('value'));
             $clientName = $client->name;
-
-            // If the active team is a client's-default team get all members
-            if ($is_default_team) {
-                $users = User::with(['teams', 'home_location'])
-                    ->filter($request->only($filterKeys))->sort()
-                    ->paginate(10)
-                    ->appends(request()->except('page'));
-            } else {
-                // else - get the members of that team
-                $team_users = TeamUser::whereTeamId($current_team->id)->get();
-                $user_ids = [];
-                foreach ($team_users as $team_user) {
-                    $user_ids[] = $team_user->user_id;
-                }
-                $users = User::whereIn('users.id', $user_ids)
-                    ->with(['teams', 'home_location'])
-                    ->filter($request->only($filterKeys))
-                    ->sort()
-                    ->paginate(10)
-                    ->appends(request()->except('page'));
-            }
-
-            foreach ($users as $idx => $user) {
-                if ($user->getRole()) {
-                    $users[$idx]->role = $user->getRole();
-                }
-                $users[$idx]['emergency_contact'] = UserDetails::whereUserId($user->id)->whereField('emergency_contact')->get()->toArray();
-                $users[$idx]->home_team = $user->getDefaultTeam();
-            }
-        } else {
-            //cb team selected
-            $session_team = session()->get('current_team');
-            if ($session_team && array_key_exists('id', $session_team)) {
-                $team = Team::find($session_team['id']);
-            } else {
-                $team = Team::find($request->user()->default_team_id);
-            }
-
-            $users = User::with('home_location')->whereHas('teams', function ($query) use ($request, $team) {
-                return $query->where('teams.id', '=', $team->id);
-            })->filter($request->only($filterKeys))->sort()
-                ->paginate(10)->appends(request()->except('page'));
-
-            foreach ($users as $idx => $user) {
-                $users[$idx]->role = $user->getRole();
-                $default_team = $user->getDefaultTeam();
-                $users[$idx]->home_team = $default_team->name;
-                $users[$idx]['emergency_contact'] = UserDetails::whereUserId($user->id)->whereField('emergency_contact')->get()->toArray();
-            }
         }
 
-        //THIS DOESN'T WORK BECAUSE OF PAGINATION BUT IT MAKES IT LOOK LIKE IT'S WORKING FOR NOW
-        //MUST FIX BY DEMO 6/15/22
-        //THIS BLOCK HAS TO BE REMOVED & QUERIES REWRITTEN WITH JOINS SO ACTUAL SORTING WORKS WITH PAGINATION
-        if ($request->get('sort') != '') {
-            if ($request->get('dir') == 'DESC') {
-                $sortedResult = $users->getCollection()->sortByDesc($request->get('sort'))->values();
-            } else {
-                $sortedResult = $users->getCollection()->sortBy($request->get('sort'))->values();
-            }
-            $users->setCollection($sortedResult);
-        }
 
         return Inertia::render('Users/Show', [
             'filters' => $request->all($filterKeys),
@@ -181,7 +120,7 @@ class UsersController extends Controller
         $userData = $user->toArray();
         $userData['all_notes'] = $user->notes->toArray();
         foreach ($userData['all_notes'] as $key => $value) {
-            if (ReadReceipt::whereNoteId($userData['all_notes'][$key]['id'])->first()) {
+            if (ReadReceipt::whereNoteId($userData['FUSall_notes'][$key]['id'])->first()) {
                 $userData['all_notes'][$key]['read'] = true;
             } else {
                 $userData['all_notes'][$key]['read'] = false;
@@ -191,7 +130,6 @@ class UsersController extends Controller
         $userData['role_id'] = $user->role()->id;
 
         return Inertia::render('Users/Edit', [
-            'selectedUser' => $userData,
             'roles' => $roles,
             'locations' => $locations,
             'availablePositions' => Position::whereClientId($client_id)->with('departments')->select('id', 'name')->get(),

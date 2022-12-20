ce0e3b37e7e920b0cbcb98bdf337717638a0416d
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2022-12-21 05:38:37 +0600

    Bugfix/20221129 end user update needs to propagate (#349)
    * Fixes #310
    
    * Undo changes to web routes file
    
    * Propagate changes on end users to member/lead/customer
    
    * Fix end user type when lead on end user update
    
    * Propagate enduser update to connected entities
    
    * Refactor end user update propagation to remove loops and utilize functions
    
    * Refactor onEndUserTrashed function in EndUserCrudProjector to use defined end user checks
    
    * Refactor onEndUserUpdated in EndUserCrudProjector to be executed within a db transaction closure
    
    * Fixed owner update in lead form and phone number reuired error
    
    * Change validation requireds to sometimes
    
    * Use count to determine if records in leads/members/customers table exists in EndUserCrudProjector
    
    * Use count to determine of record exists in leads/members/customers table
    
    * Remove passing should_update_email to updateEndUserEntity in EndUserCrudProjector
    
    * Refactor code to reduce code duplication
    
    * Refactor end user creation to reduce code duplication
    
    Co-authored-by: Sovon Saha <saha.sovon89@gmail.com>
    Co-authored-by: CnBlair <104932195+CnBlair@users.noreply.github.com>

--- app/Domain/EndUsers/Projectors/EndUserCrudProjector.php
+++ app/Domain/EndUsers/Projectors/EndUserCrudProjector.php
@@ -22,43 +22,40 @@ class EndUserCrudProjector extends Projector
     public function onEndUserCreated(EndUserCreated $event): void
     {
         DB::transaction(function () use ($event) {
-            $end_user = (new EndUser())->writeable();
-            $fillable_data = array_filter($event->payload, function ($key) use ($end_user) {
-                return in_array($key, $end_user->getFillable());
-            }, ARRAY_FILTER_USE_KEY);
-            $end_user->id = $event->aggregateRootUuid();
-            $end_user->client_id = $event->payload['client_id'];
-            $end_user->email = $event->payload['email'];
-            $end_user->fill($fillable_data);
+            $end_user = $this->createEndUserEntity($event, (string) EndUser::class);
 
-            $end_user->save();
+            $this->setEndUserEntity($event, $end_user);
 
             $this->createOrUpdateEndUserNotes($event, $end_user);
 
             //ON END USER CREATION, END USER IS AUTOMATICALLY A LEAD NO MATTER WHAT
-            $end_user = (new Lead())->writeable();
-            $fillable_data = array_filter($event->payload, function ($key) use ($end_user) {
-                return in_array($key, $end_user->getFillable());
-            }, ARRAY_FILTER_USE_KEY);
-            $end_user->id = $event->aggregateRootUuid();
-            $end_user->client_id = $event->payload['client_id'];
-            $end_user->email = $event->payload['email'];
-            $end_user->fill($fillable_data);
+            $end_user = $this->createEndUserEntity($event, (string) Lead::class);
 
-            $end_user->save();
+            $this->setEndUserEntity($event, $end_user);
         });
     }
 
     public function onEndUserUpdated(EndUserUpdated $event): void
     {
-        $end_user = EndUser::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
-        if (array_key_exists('email', $event->payload)) {
-            $end_user->email = $event->payload['email'];
-        }
-        $end_user->fill($event->payload);
-        $end_user->save();
+        DB::transaction(function () use ($event) {
+            $end_user = EndUser::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
+            $this->setEndUserEntity($event, $end_user);
 
-        $this->createOrUpdateEndUserNotes($event, $end_user);
+            $this->createOrUpdateEndUserNotes($event, $end_user);
+
+            if ($this->isPotentialLead($event)) {
+                $end_user = Lead::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
+                $this->setEndUserEntity($event, $end_user);
+            }
+            if ($this->isPotentialCustomer($event)) {
+                $end_user = Customer::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
+                $this->setEndUserEntity($event, $end_user);
+            }
+            if ($this->isPotentialMember($event)) {
+                $end_user = Member::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
+                $this->setEndUserEntity($event, $end_user);
+            }
+        });
     }
 
     public function onEndUserTrashed(EndUserTrashed $event): void
@@ -67,17 +64,13 @@ class EndUserCrudProjector extends Projector
             $end_user = EndUser::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable();
             $end_user->delete();
 
-            $potentialLead = Lead::withTrashed()->find($event->aggregateRootUuid()); //check to see if in leads table
-            $potentialCustomer = Customer::withTrashed()->find($event->aggregateRootUuid()); //check to see if in customers table
-            $potentialMember = Member::withTrashed()->find($event->aggregateRootUuid()); //check to see if in members table
-
-            if ($potentialLead) {
+            if ($this->isPotentialLead($event)) {
                 Lead::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable()->delete();
             }
-            if ($potentialCustomer) {
+            if ($this->isPotentialCustomer($event)) {
                 Customer::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable()->delete();
             }
-            if ($potentialMember) {
+            if ($this->isPotentialMember($event)) {
                 Member::withTrashed()->findOrFail($event->aggregateRootUuid())->writeable()->delete();
             }
         });
@@ -116,5 +109,56 @@ class EndUserCrudProjector extends Projector
                 'created_by_user_id' => $event->modifiedBy(),
             ]);
         }
+    }
+
+    /**
+     * Check to see if enduser is in leads table
+     */
+    protected function isPotentialLead($event): bool
+    {
+        return count(Lead::withTrashed()->whereId($event->aggregateRootUuid())->get());
+    }
+
+    /**
+     * Check to see if enduser is in customers table
+     */
+    protected function isPotentialCustomer($event): bool
+    {
+        return count(Customer::withTrashed()->whereId($event->aggregateRootUuid())->get());
+    }
+
+    /**
+     * Check to see if enduser is in members table
+     */
+    protected function isPotentialMember($event): bool
+    {
+        return count(Member::withTrashed()->whereId($event->aggregateRootUuid())->get());
+    }
+
+    protected function createEndUserEntity($event, string $entity): EndUser
+    {
+        $end_user = null;
+
+        if ($entity == (string) EndUser::class) {
+            $end_user = (new EndUser())->writeable();
+        } elseif ($entity == (string) Lead::class) {
+            $end_user = (new Lead())->writeable();
+        } elseif ($entity == (string) Member::class) {
+            $end_user = (new Member())->writeable();
+        } else {
+            $end_user = (new Customer())->writeable();
+        }
+
+        $end_user->id = $event->aggregateRootUuid();
+        $end_user->client_id = $event->payload['client_id'];
+
+        return $end_user;
+    }
+
+    protected function setEndUserEntity($event, EndUser $end_user): void
+    {
+        $end_user->email = $event->payload['email'] ?? $end_user->email;
+        $end_user->fill($event->payload);
+        $end_user->save();
     }
 }

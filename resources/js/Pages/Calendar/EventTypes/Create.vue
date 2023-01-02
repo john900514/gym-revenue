bc817b7935f59fea460a3a9c302e4d583bae5793
David Duncan <devduncan89@gmail.com>
2023-01-02 12:06:51 -0500

    calendar event type create - remove inertia modal & refactor to script setup

--- resources/js/Pages/Calendar/EventTypes/Create.vue
+++ resources/js/Pages/Calendar/EventTypes/Create.vue
@@ -1,45 +1,12 @@
 <template>
-    <ModalableWrapper>
-        <LayoutHeader title="Calendar Event Types">
-            <h2 class="font-semibold text-xl leading-tight">
-                Create Event Type
-            </h2>
-        </LayoutHeader>
+    <LayoutHeader title="Calendar Event Types">
+        <h2 class="font-semibold text-xl leading-tight">Create Event Type</h2>
+    </LayoutHeader>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <ModalSlot />
-            </div>
-        </div>
-        <template #modal>
-            <calendar-event-type-form :client-id="$page.props.user.client_id" />
-        </template>
-    </ModalableWrapper>
+    <calendar-event-type-form :client-id="$page.props.user.client_id" />
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
 import LayoutHeader from "@/Layouts/LayoutHeader.vue";
-import Button from "@/Components/Button.vue";
-import JetFormSection from "@/Jetstream/FormSection.vue";
-
-import JetInputError from "@/Jetstream/InputError.vue";
-import JetLabel from "@/Jetstream/Label.vue";
 import CalendarEventTypeForm from "@/Pages/Calendar/EventTypes/Partials/CalendarEventTypeForm.vue";
-
-import { ModalableWrapper, ModalSlot } from "@/Components/InertiaModal";
-
-export default defineComponent({
-    components: {
-        CalendarEventTypeForm,
-        LayoutHeader,
-        Button,
-        JetFormSection,
-        JetInputError,
-        JetLabel,
-        ModalableWrapper,
-        ModalSlot,
-    },
-    props: {},
-});
 </script>

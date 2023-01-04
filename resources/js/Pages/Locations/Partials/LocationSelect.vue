ab2a879f8cb5618e8dc42b3a4dac59932744af5c
David Duncan <devduncan89@gmail.com>
2023-01-04 12:48:30 -0500

    locations drop down multiselect - is binding correctly to forms just teams are not updated on back end with correct values

---
+++ resources/js/Pages/Locations/Partials/LocationSelect.vue
@@ -0,0 +1,60 @@
+<template>
+    <ApolloQuery :query="(gql) => queries['locations']">
+        <template v-slot="{ result: { loading, error, data }, isLoading }">
+            <template v-if="isLoading">
+                <p>Loading...</p>
+            </template>
+
+            <template v-if="error">
+                <jet-input-error :message="error" class="mt-2" />
+            </template>
+
+            <template v-if="data">
+                <jet-label for="location" value="Locations" />
+                <Multiselect
+                    v-bind="$attrs"
+                    id="location"
+                    class="mt-1 multiselect"
+                    mode="tags"
+                    v-model="localValue"
+                    :close-on-select="false"
+                    :create-option="true"
+                    :options="formatLocationSelect(data.locations.data)"
+                    :classes="getDefaultMultiselectTWClasses()"
+                />
+            </template>
+        </template>
+    </ApolloQuery>
+</template>
+
+<script setup>
+import { ref, defineEmits, watch } from "vue";
+import queries from "@/gql/queries";
+import { formatLocationTypeForSelect } from "@/utils/locationTypeEnum";
+import { getDefaultMultiselectTWClasses } from "@/utils";
+import JetLabel from "@/Jetstream/Label.vue";
+import JetInputError from "@/Jetstream/InputError.vue";
+import Multiselect from "@vueform/multiselect";
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: "",
+    },
+});
+
+const formatLocationSelect = (data) => {
+    if (!data instanceof Array) return [];
+    return data.map((location) => {
+        return {
+            value: location.id,
+            label: location.name,
+        };
+    });
+};
+
+const localValue = ref(props.modelValue);
+const emit = defineEmits(["update:modelValue"]);
+
+watch(localValue, (nv, ov) => emit("update:modelValue", nv));
+</script>
--- resources/js/Pages/Teams/Partials/TeamForm.vue
+++ resources/js/Pages/Teams/Partials/TeamForm.vue
@@ -7,7 +7,7 @@
         </template>
 
         <template #form>
-            <div class="col-span-6 sm:col-span-4">
+            <div class="grid-col-full">
                 <jet-label for="name" value="Team Name" />
                 <input
                     id="name"
@@ -19,29 +19,8 @@
                 <jet-input-error :message="form.errors.name" class="mt-2" />
             </div>
 
-            <div
-                class="col-span-6 sm:col-span-4"
-                v-if="availableLocations?.data.length"
-            >
-                <jet-label for="locations" value="Locations" />
-                <multiselect
-                    v-model="form.locations"
-                    id="locations"
-                    mode="tags"
-                    :close-on-select="false"
-                    :create-option="true"
-                    :options="
-                        availableLocations.data.map((location) => ({
-                            label: location.name,
-                            value: location.id,
-                        }))
-                    "
-                    :classes="multiselectClasses"
-                />
-                <jet-input-error
-                    :message="form.errors.locations"
-                    class="mt-2"
-                />
+            <div class="grid-col-full">
+                <LocationSelect v-model="form.locations" />
             </div>
         </template>
 
@@ -60,83 +39,64 @@
     </jet-form-section>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import { usePage } from "@inertiajs/inertia-vue3";
+<style scoped>
+.grid-col-full {
+    grid-column: 1 / -1;
+}
+</style>
+
+<script setup>
+import * as _ from "lodash";
+import mutations from "@/gql/mutations";
 import { useGymRevForm } from "@/utils";
+import { useMutation } from "@vue/apollo-composable";
+
+import LocationSelect from "@/Pages/Locations/Partials/LocationSelect.vue";
 import Button from "@/Components/Button.vue";
 import JetFormSection from "@/Jetstream/FormSection.vue";
 import JetActionMessage from "@/Jetstream/ActionMessage.vue";
 import JetInputError from "@/Jetstream/InputError.vue";
 import JetLabel from "@/Jetstream/Label.vue";
-import Multiselect from "@vueform/multiselect";
-import { getDefaultMultiselectTWClasses } from "@/utils";
-import * as _ from "lodash";
-import { useMutation } from "@vue/apollo-composable";
-import mutations from "@/gql/mutations";
 
-export default defineComponent({
-    components: {
-        Button,
-        JetFormSection,
-        JetInputError,
-        JetLabel,
-        JetActionMessage,
-        Multiselect,
-    },
-    props: {
-        team: {
-            type: Object,
-        },
-        availableLocations: {
-            type: Object,
-            required: true,
-        },
-    },
-
-    setup(props, { emit }) {
-        const page = usePage();
-        let operation = "Update";
-        let team = _.cloneDeep(props.team);
-        if (!team) {
-            team = {
-                name: "",
-                locations: [],
-            };
-            operation = "Create";
-        } else {
-            team.locations = team.locations.map((detail) => detail.value);
-        }
-        const form = useGymRevForm(team);
-
-        const { mutate: createTeam } = useMutation(mutations.team.create);
-        const { mutate: updateTeam } = useMutation(mutations.team.update);
-
-        let handleSubmit = async () => {
-            await updateTeam({
-                id: team.id,
-                name: form.name,
-                positions: form.positions,
-            });
-            emit("close");
-        };
-        if (operation === "Create") {
-            handleSubmit = async () => {
-                await createTeam({
-                    name: form.name,
-                    positions: form.positions,
-                });
-                emit("close");
-            };
-        }
-
-        return {
-            form,
-            operation,
-            handleSubmit,
-            page,
-            multiselectClasses: getDefaultMultiselectTWClasses(),
-        };
+const props = defineProps({
+    team: {
+        type: Object,
     },
 });
+
+const emit = defineEmits(["close"]);
+
+let operation = "Update";
+let team = _.cloneDeep(props.team);
+if (!team) {
+    team = {
+        name: "",
+        locations: [],
+    };
+    operation = "Create";
+} else {
+    team.locations = team.locations.map((detail) => detail.value);
+}
+const form = useGymRevForm(team);
+
+const { mutate: createTeam } = useMutation(mutations.team.create);
+const { mutate: updateTeam } = useMutation(mutations.team.update);
+
+let handleSubmit = async () => {
+    await updateTeam({
+        id: team.id,
+        name: form.name,
+        positions: form.positions,
+    });
+    emit("close");
+};
+if (operation === "Create") {
+    handleSubmit = async () => {
+        await createTeam({
+            name: form.name,
+            positions: form.positions,
+        });
+        emit("close");
+    };
+}
 </script>

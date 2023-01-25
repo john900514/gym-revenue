4f62018dff38b2b76c04efbf08fdc278c07881b7
pravinbacancy <86301794+pravinbacancy@users.noreply.github.com>
2023-01-25 17:18:41 +0530

    made structured document and structured document file versionable and… (#544)
    * made structured document and structured document file versionable and change entity_type validation #317
    
    * updated versionable namespace in StructuredDocument and StructuredDocumentFile Projection #317
    
    Co-authored-by: CnBlair <104932195+CnBlair@users.noreply.github.com>

--- app/Domain/StructuredDocuments/Actions/CreateStructuredDocument.php
+++ app/Domain/StructuredDocuments/Actions/CreateStructuredDocument.php
@@ -6,10 +6,12 @@ namespace App\Domain\StructuredDocuments\Actions;
 
 use App\Domain\StructuredDocuments\Projections\StructuredDocument;
 use App\Domain\StructuredDocuments\StructuredDocumentAggregate;
+use App\Enums\StructuredDocumentEntityTypeEnum;
 use App\Http\Middleware\InjectClientId;
 use App\Support\Uuid;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rules\Enum;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Prologue\Alerts\Facades\Alert;
@@ -28,7 +30,7 @@ class CreateStructuredDocument
         return [
             'client_id' => ['required', 'string', 'exists:clients,id'],
             'entity_id' => ['required', 'string'],
-            'entity_type' => ['required', 'string'],
+            'entity_type' => ['required', 'string', new Enum(StructuredDocumentEntityTypeEnum::class)],
             'template_file_id' => ['sometimes', 'string'],
             'ttl' => 'required',
         ];

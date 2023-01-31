1b01d03c9ba24c741be86cc5995db59cd8ac9d2e
philip <philip@capeandbay.com>
2023-01-31 14:46:39 -0500

    bunch of reorg to fix import issues. not final, but if it unblocks us for now, I'll take it.

---
+++ app/Actions/Support/DiscoverDomainGraphql.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Support;
+
+use Illuminate\Support\Str;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class DiscoverDomainGraphql
+{
+    use asAction;
+
+    public function handle(): \Illuminate\Support\Collection
+    {
+        $files = (new Finder())->files()->in(app()->path());
+
+        return collect($files)
+            ->reject(fn (SplFileInfo $file) => $file->getExtension() !== 'gql')
+            ->reject(fn (SplFileInfo $file) => ! str($file->getFilename())->contains('schema'))
+//            ->map(fn (SplFileInfo $file) => $this->fullQualifiedClassNameFromFile($file))
+//            ->filter(fn (string $class) => $this->checkIsAction($class))
+//            ->filter(fn (string $class) => (new \ReflectionClass($class))->isInstantiable())
+            ->map(fn (string $class) => str($class)->explode('\\')->implode('\\'))
+            ->values();
+    }
+
+    private function fullQualifiedClassNameFromFile(SplFileInfo $file): string
+    {
+        $class = trim(Str::replaceFirst(base_path(), '', $file->getRealPath()), DIRECTORY_SEPARATOR);
+
+        return str_replace(
+            [DIRECTORY_SEPARATOR, 'App\\'],
+            ['\\', app()->getNamespace()],
+            ucfirst(Str::replaceLast('.php', '', $class))
+        );
+    }
+}

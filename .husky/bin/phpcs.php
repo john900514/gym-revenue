aa7ef1241c8befb251b7c89eb8ca312d7ede0452
Chrys Ugwu <chrys@capeandbay.com>
2023-02-10 12:35:18 -0600

    fix composer

--- .husky/bin/phpcs.php
+++ .husky/bin/phpcs.php
@@ -6,6 +6,10 @@ use PHP_CodeSniffer\Runner;
 include_once __DIR__ . '/../../vendor/squizlabs/php_codesniffer/autoload.php';
 
 exec('git diff --cached --name-only --diff-filter=ACMR HEAD | grep .php', $changed_files);
+if (!empty($changed_files)) {
+    return;
+}
+
 $_SERVER['argv'] = [
     './vendor/bin/phpcs',
     '--standard=.quality/phpcs.xml',

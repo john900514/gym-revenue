e5fafb1764868a1618aa2e2654a8de3f42b5747c
Chrys Ugwu <chrys@capeandbay.com>
2023-02-10 12:38:55 -0600

    fix composer

--- .husky/bin/phpcs.php
+++ .husky/bin/phpcs.php
@@ -6,7 +6,7 @@ use PHP_CodeSniffer\Runner;
 include_once __DIR__ . '/../../vendor/squizlabs/php_codesniffer/autoload.php';
 
 exec('git diff --cached --name-only --diff-filter=ACMR HEAD | grep .php', $changed_files);
-if (!empty($changed_files)) {
+if (empty($changed_files)) {
     return;
 }
 
